b238f2e522d4c812a8fdff255c68e5e238aa0b14
tadiwanashedombe <dombeseant@gmail.com>
2026-07-03 06:18:35 +0200

    js adiition

---
+++ scripts/gatedates.js
@@ -0,0 +1,9 @@
+// select elements to edit
+const year = document.querySelector("#currentyear");
+
+
+
+year.textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
