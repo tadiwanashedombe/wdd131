916b75f0c84bdcf1dc76d9e2dd2ffcbf33098178
tadiwanashedombe <dombeseant@gmail.com>
2026-07-10 23:50:33 +0200

    week2: flex,positioning,grid,fonts,hamburger menu,.js cllassList

---
+++ week02/scripts/temples.js
@@ -0,0 +1,9 @@
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
