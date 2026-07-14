e59546c3af94305537835e27ebe911e95ad349e3
tadiwanashedombe <dombeseant@gmail.com>
2026-07-14 13:05:09 +0200

    html,large-css complete

---
+++ scripts/place.js
@@ -0,0 +1,7 @@
+// select elements to edit
+const year = document.querySelector("#currentyear");
+
+
+
+year.textContent = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = document.lastModified;
